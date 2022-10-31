2bef34af345627ac902a4a80b615f4340eb73bab
Robert Krampe <robert.krampe@gmx.net>
2022-10-31 20:05:58 +0100

    add dlugin base php file with header information

---
+++ rk-yt2c.php
@@ -0,0 +1,9 @@
+/**
+ * Plugin Name:     YouTube 2-Click-Solution
+ * Description:     GDPR conform solution to embed YouTube Videos
+ * Version:         0.1
+ * Author:          Robert Krampe
+ * Author URI:      https://github.com/robertk44
+ * Text Domain:     rk-yt2c
+ * Domain Path:     /languages
+ */
