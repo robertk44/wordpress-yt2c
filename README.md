7e6e8cb17fdeece5706bf6b02be1e7e4cd22a480
Robert Krampe <robert.krampe@gmx.net>
2022-11-05 14:45:05 +0100

    v0.1 show Button instead of embedded video

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # wordpress-yt2c
 YouTube Two-Click-Solution Wordpress Plugin
+
+# Changelog
+
+## v0.1
+
+Basis-Variante: YouTube-Videos werden nicht mehr eingebettet sondern stattdessen ein Link angezeigt, der das Video auf YouTube in einem neuen Fenster/Tab öffnet.
+
+### Geplante Updates:
+
+- statt dem Video ein Platzhalter-Bild anzeigen mit Hinweistext, dass bei Klick Daten an YouTube gesendet werden => Video in iframe anzeigen (datensparsam über youtube-nocookie.com)
+- Checkbox "immer anzeigen" (dieses Cookie ist technisch notwendig)
--- rk-yt2c.php
+++ rk-yt2c.php
@@ -1,3 +1,4 @@
+<?php
 /**
  * Plugin Name:     YouTube 2-Click-Solution
  * Description:     GDPR conform solution to embed YouTube Videos
@@ -7,3 +8,48 @@
  * Text Domain:     rk-yt2c
  * Domain Path:     /languages
  */
+
+defined('ABSPATH') or die('');
+
+define('COMBY_PLUGIN_PATH', plugin_dir_path(__FILE__));
+define('COMBY_PLUGIN_URL', plugin_dir_url(__FILE__));
+
+// Plugin Activation Hook
+register_activation_hook(__FILE__, 'rk_yt2c_activate');
+function rk_yt2c_activate()
+{
+}
+
+// init plugin
+add_action('plugins_loaded', 'rk_yt2c_init');
+function rk_yt2c_init()
+{
+    // load language files
+    // load_plugin_textdomain('rk_yt2c', false, basename(dirname(__FILE__)) . '/languages');
+}
+
+function rk_yt2c_the_content($content)
+{
+    // html template
+    $template = '<div class="rk-yt2c-ytvideo">
+        <div class="rk-yt2c-ytvideo__overlay"></div>
+        <div class="rk-yt2c-ytvideo__content" data-video-id="$2" data-video-url="$0"></div>
+        <a href="$0" target="_blank" class="button">Video auf YouTube öffnen (neues Fenster/Tab)</a>
+        </div>';
+    // search for youtube links
+    $content = preg_replace('/https?\:\/\/(www.)?youtube.com\/watch\?v\=([A-z0-9]+)/i', $template, $content);
+
+    return $content;
+}
+add_filter('the_content', 'rk_yt2c_the_content');
+
+add_filter('embed_oembed_html', 'rk_yt2c_process_oembed_html', 99, 4);
+function rk_yt2c_process_oembed_html($cached_html, $url, $attr, $post_id)
+{
+    // disable YouTube embed and return URL only instead
+    if (false !== strpos($url, "youtube.com") || false !== strpos($url, "youtu.be")) {
+        return $url;
+    }
+
+    return $cached_html;
+}
